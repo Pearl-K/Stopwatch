f97243f906aceaaf9350de5c26b26ab265602918
Pearl <rkdwlswn227@gmail.com>
2024-03-12 23:14:55 +0900

    edit: main activity

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -2,10 +2,57 @@ package com.example.stopwatch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btn_start : Button
+    private lateinit var btn_refresh : Button
+    private lateinit var tv_minute : TextView
+    private lateinit var tv_second : TextView
+    private lateinit var tv_millisecond : TextView
+
+    private var isRunning = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_start = findViewById(R.id.btn_start)
+        btn_refresh = findViewById(R.id.btn_restart)
+        tv_minute = findViewById(R.id.tv_minute)
+        tv_second = findViewById(R.id.tv_second)
+        tv_millisecond = findViewById(R.id.tv_millsecond)
     }
+
+    private fun start(){
+
+    }
+
+    private fun pause(){
+
+    }
+
+    private fun refresh(){
+
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id){
+            R.id.btn_start -> {
+                if(isRunning){
+                    pause()
+                } else {
+                    start()
+                }
+            }
+
+            R.id.btn_restart -> {
+                refresh()
+            }
+        }
+    }
+
 }
